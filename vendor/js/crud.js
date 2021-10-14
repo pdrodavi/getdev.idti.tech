b37a67b7c04232cb32b62ce89b1c0abd5cb40b71
Pedro Davi <pdrodavi@outlook.com>
2021-10-13 22:02:28 -0300

    add fields

--- vendor/js/crud.js
+++ vendor/js/crud.js
@@ -20,8 +20,41 @@ devform.onsubmit = function (event) {
     var devRef = firebase.database().ref('devs/' + userId);
 
     return devRef.once('value').then((snapshot) => {
+
       var username = (snapshot.val()) || 'Anonymous';
-      console.log(username)
+      
+      if (username == null) {
+
+        var data = {
+          firstname: devform.firstName.value,
+          lastname: devform.lastName.value,
+          username: devform.username.value,
+          email: devform.email.value,
+          country: devform.country.value,
+          state: devform.state.value,
+          zipcode: devform.zip.value,
+          education: devform.education.value,
+          edustate: devform.edustate.value,
+          edunivel: devform.edunivel.value,
+          edustart: devform.edustart.value,
+          eduend: devform.eduend.value,
+          academy: devform.academy.value,
+          acting: devform.acting.value,
+          primarylang: devform.primarylang.value,
+          secondarylang: devform.secondarylang.value,
+          secondarylang: devform.secondarylang.value
+        }
+  
+        dbRefUsers.child(data.username).push(data).then(function () {
+          console.log(data)
+        }).catch(function (error) {
+          console.log('Error: ', error)
+        })
+
+      } else {
+        alert('Usuário já existe!')
+      }
+    
     });  
 
   } else {
@@ -30,31 +63,7 @@ devform.onsubmit = function (event) {
 
     if (devform.username.value != '') {
 
-      var data = {
-        firstname: devform.firstName.value,
-        lastname: devform.lastName.value,
-        username: devform.username.value,
-        email: devform.email.value,
-        country: devform.country.value,
-        state: devform.state.value,
-        zipcode: devform.zip.value,
-        education: devform.education.value,
-        edustate: devform.edustate.value,
-        edunivel: devform.edunivel.value,
-        edustart: devform.edustart.value,
-        eduend: devform.eduend.value,
-        academy: devform.academy.value,
-        acting: devform.acting.value,
-        primarylang: devform.primarylang.value,
-        secondarylang: devform.secondarylang.value,
-        secondarylang: devform.secondarylang.value
-      }
-
-      dbRefUsers.child(data.username).push(data).then(function () {
-        console.log(data)
-      }).catch(function (error) {
-        console.log('Error: ', error)
-      })
+      
 
     } else {
       alert('O username Github é obrigatório!')
