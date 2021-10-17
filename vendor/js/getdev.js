cffceb858c403caf07464d8144acae9e369e598b
Pedro Davi <pdrodavi@outlook.com>
2021-10-17 07:40:05 -0300

    plus

--- vendor/js/getdev.js
+++ vendor/js/getdev.js
@@ -27,28 +27,14 @@ getdevform.onsubmit = function (event) {
       
       if (username == 'Anonymous') {
         alert('Dev não existe na base!')
+        window.location.replace("getdev.html");
       } else {
-        //var doc = new jsPDF(); 
-        
-        //ulTodoList.innerHTML = ''
-        //var num = snapshot.numChildren()
-
         snapshot.forEach(function (item) { // Percorre todos os elementos
             var value = item.val()
-            //var li = document.createElement('li') // Cria um elemento do tipo li
-            //var spanLi = document.createElement('span') // Cria um elemento do tipo span
-
             firstName.innerHTML = value.firstname
             userEmail.innerHTML = value.email
             userName.innerHTML = value.username
-
-            //spanLi.appendChild(document.createTextNode(value.username)) // Adiciona o elemento de texto dentro da nossa span
-            //li.appendChild(spanLi) // Adiciona o span dentro do li
-            //ulTodoList.appendChild(li) // Adiciona o li dentro da lista de tarefas
-            //doc.text(JSON.stringify(value.username), 20, 20);
-            //console.log(value)
           })
-          //doc.save('a4.pdf')
       }
 
       hideItem(getDev)
